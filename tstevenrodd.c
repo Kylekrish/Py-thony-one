9fae02b8267a6fffac03405aaadf657eda619cfe
Murali Krishna <42571165+Kylekrish@users.noreply.github.com>
2018-12-15 10:58:36 +0530

    tstevenrodd

---
+++ tstevenrodd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{   int k;
+    printf("enter a number:\n");
+    scanf("%d",&k);
+    if(k%2==0)
+    printf("the given number is even");
+    else
+    printf("the given number is odd");
+
+    return 0;
+}
